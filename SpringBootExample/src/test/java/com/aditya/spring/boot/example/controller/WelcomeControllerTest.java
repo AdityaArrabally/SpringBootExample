b587ad4ffbdc44778a740a8f18f6dd355bffeaac
aditya arrabally <aditya.arrabally@gmail.com>
2022-02-08 14:42:08 -0500

    Commit.

--- SpringBootExample/src/test/java/com/aditya/spring/boot/example/controller/WelcomeControllerTest.java
+++ SpringBootExample/src/test/java/com/aditya/spring/boot/example/controller/WelcomeControllerTest.java
@@ -13,47 +13,39 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.servlet.ModelAndView;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(controllers = WelcomeController.class)
 public class WelcomeControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    List<String> expectedList = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+	List<String> expectedList = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
 
-    @Test
-    public void main() throws Exception {
-        ResultActions resultActions = mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("welcome"))
-                .andExpect(model().attribute("message", equalTo("Aditya")))
-                .andExpect(model().attribute("tasks", is(expectedList)))
-                .andExpect(content().string(containsString("Hello, Aditya")));
+	@Test
+	public void main() throws Exception {
+		ResultActions resultActions = mockMvc.perform(get("/")).andExpect(status().isOk())
+				.andExpect(view().name("welcome")).andExpect(model().attribute("message", equalTo("Aditya")))
+				.andExpect(model().attribute("tasks", is(expectedList)))
+				.andExpect(content().string(containsString("Hello, Aditya")));
 
-        MvcResult mvcResult = resultActions.andReturn();
-        ModelAndView mv = mvcResult.getModelAndView();
-        //
-    }
+		MvcResult mvcResult = resultActions.andReturn();
+		ModelAndView mv = mvcResult.getModelAndView();
+		//
+	}
 
-    // Get request with Param
-    @Test
-    public void hello() throws Exception {
-        mockMvc.perform(get("/hello").param("name", "I Love Kotlin!"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("welcome"))
-                .andExpect(model().attribute("message", equalTo("I Love Kotlin!")))
-                .andExpect(content().string(containsString("Hello, I Love Kotlin!")));
-    }
-
+	// Get request with Param
+	@Test
+	public void hello() throws Exception {
+		mockMvc.perform(get("/hello").param("name", "I Love Kotlin!")).andExpect(status().isOk())
+				.andExpect(view().name("welcome")).andExpect(model().attribute("message", equalTo("I Love Kotlin!")))
+				.andExpect(content().string(containsString("Hello, I Love Kotlin!")));
+	}
 
 }
